a349c7e5545847336bb0c5f58048b2b785618312
swethakeerthipati <swethakeerthipati@gmail.com>
2023-06-15 23:45:38 +0200

    Add React challenges

---
+++ react-basics_attributes/src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <article>Replace me with an article</article>;
+}
---
+++ react-basics_hello-world-article/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello world!</h1>
+      <p>This is a paragraph.</p>
+    </article>
+  );
+}
+
+export default function App() {
+  return <HelloWorldArticle />;
+}
