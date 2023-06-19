eec327d2c8eba6ec7f5f3b8ea1095c3ae251a1b1
swethakeerthipati <swethakeerthipati@gmail.com>
2023-06-20 01:01:04 +0200

    Add React State Challenges

---
+++ react-state_emoji-checker/src/App.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+export default function App() {
+  //let code = "?";
+  const [code, setCode] = useState("?");
+  const validCode = "🐡🐠🐋";
+
+  function handleClick(emoji) {
+    setCode(code + emoji);
+  }
+  return (
+    <div className="container">
+      <div className="button-container">
+        <button
+          type="button"
+          onClick={() => {
+            handleClick("🐡");
+            console.log("Update Code!");
+          }}
+        >
+          <span role="img" aria-label="Pufferfish">
+            🐡
+          </span>
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            handleClick("🐋");
+            console.log("Update Code!");
+          }}
+        >
+          <span role="img" aria-label="Whale">
+            🐋
+          </span>
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            handleClick("🐠");
+            console.log("Update Code!");
+          }}
+        >
+          <span role="img" aria-label="Clownfish">
+            🐠
+          </span>
+        </button>{" "}
+      </div>
+
+      <button
+        type="button"
+        onClick={() => {
+          setCode("");
+          console.log("Reset Code!");
+        }}
+      >
+        Reset
+      </button>
+      <h2>{code}</h2>
+
+      {code === validCode && <p>Valid code!</p>}
+    </div>
+  );
+}
